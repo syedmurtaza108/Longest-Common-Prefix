a9e3116039561dba9e782638e30ff74830c0127e
syedmurtaza108 <murtaza.hussain@koderlabs.com>
2021-10-30 18:18:27 +0500

    Add project files.

---
+++ Longest Common Prefix/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Longest_Common_Prefix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
